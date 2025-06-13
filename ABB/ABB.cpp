2b8951c0ec6d7d2395fa0720486d4c347dc6f7b8
GuilhermeHorta22 <ghorta800@gmail.com>
2025-06-13 14:18:15 -0300

    alterações no arquivo de abb

--- ABB/ABB.cpp
+++ ABB/ABB.cpp
@@ -10,6 +10,53 @@ struct tree
 };
 typedef struct tree Tree;
 
+struct TpPilha
+{
+	Tree *info;
+	struct TpPilha *prox;
+};
+typedef struct TpPilha Pilha;
+
+void init(Pilha **p)
+{
+	*p = NULL;
+}
+
+char isEmpty(Pilha *p)
+{
+	return p == NULL;
+}
+
+Tree *top(Pilha *p)
+{
+	if(!isEmpty(p))
+		return p->info;
+	else
+		return NULL;
+}
+
+void push(Pilha **p, Tree *info)
+{
+	Pilha *nova = (Pilha*)malloc(sizeof(Pilha));;
+	nova->info = info;
+	
+	nova->prox = *p;
+	*p = nova;
+}
+
+Tree *pop(Pilha **p)
+{
+	if(!isEmpty(*p))
+	{
+		Pilha *aux = *p;
+		Tree *ret = aux->info;
+		*p = aux->prox;
+		free(aux);
+		return ret;
+	}
+	return NULL;
+}
+
 void initTree(Tree **raiz)
 {
 	*raiz = NULL;
@@ -40,7 +87,24 @@ void pre_ordem(Tree *raiz)
 }
 
 //travessia em pre ordem INTERATIVO
-
+void pre_ordemInt(Tree *raiz)
+{
+	Pilha *p;
+	init(&p);
+	push(&p,raiz);
+	Tree *atual = NULL;
+	
+	while(!isEmpty(p))
+	{
+		atual = pop(&p);
+		printf("%d\t",atual->info);
+		
+		if(raiz->dir != NULL)
+			push(&p,atual->dir);
+		if(raiz->esq != NULL)
+			push(&p,atual->esq);
+	}
+}
 
 //travessia em in ordem RECURSIVO
 void in_ordem(Tree *raiz)
@@ -54,7 +118,10 @@ void in_ordem(Tree *raiz)
 }
 
 //travessia em in ordem INTERATIVO
-
+void in_ordemInt(Tree *raiz)
+{
+	
+}
 
 //travessia em pos ordem RECURSIVO
 void pos_ordem(Tree *raiz)
